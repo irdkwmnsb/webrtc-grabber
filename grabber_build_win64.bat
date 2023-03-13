6f37bb7fc4463b9121985f119c43eae64b17b632
Kostya Bats <kbats@bk.ru>
2023-03-14 01:01:30 +0300

    Delete nodejs signaling, fix grabber build script, temporary delete signalling from docker-compose

--- grabber_build_win64.bat
+++ grabber_build_win64.bat
@@ -1,7 +1,7 @@
 @echo off
 cd packages\grabber
-call rmdir /S /Q build\webrtc_grabber_win64
-call npm run build_win64
-call move /Y build\grabber-win32-x64 build\webrtc_grabber_win64
-call xcopy /Y scripts\*.bat build\webrtc_grabber_win64
+call rmdir /S /Q build\webrtc_grabber_win_x64
+call npm run build_win32_x64
+call move /Y build\grabber-win32-x64 build\webrtc_grabber_win_x64
+call xcopy /Y scripts\*.bat build\webrtc_grabber_win_x64
 REM Ensure that you did npm ci in packages/grabber
