c5ad9c798d38fa4dfb7933989f1810c7e80a06ee
Maxim <me@alzhanov.ru>
2024-01-31 16:45:24 +0300

    Rework to tsup and publishing

--- packages/frontend-sdk/lib/grabber_capture.ts
+++ packages/frontend-sdk/lib/grabber_capture.ts
@@ -1,9 +1,12 @@
 import {GrabberSocket} from "./sockets";
+import {IOfferReceiveResponder, StreamType} from "./api";
 
 export class GrabberCaptureClient { // FIXME inherit from the other client
     private peerName: string;
     public target: EventTarget; // FIXME: make this private and make proper on function
     private socket: GrabberSocket<any>;
+    private offerReceiveResponder?: IOfferReceiveResponder;
+
     constructor(peerName: string, signallingUrl?: string) {
         this.peerName = peerName;
         this.target = new EventTarget();
@@ -20,6 +23,11 @@ export class GrabberCaptureClient { // FIXME inherit from the other client
             this.target.dispatchEvent(new CustomEvent('init_peer', {detail: {pcConfig, pingInterval}}));
         };
         const offer_handle = async ({offer: {peerId, offer, streamType}}: any) => { // FIXME: remove once proper types are implemented
+            // if (this.offerReceiveResponder === undefined) {
+            //     console.warn("Got an incomming call, even though onOfferReceived is not called.");
+            //     return;
+            // }
+            // await this.offerReceiveResponder(peerId, offer, streamType);
             this.target.dispatchEvent(new CustomEvent('offer', {detail: {playerId: peerId, offer, streamType}}));
         }
         const player_ice_handle = async ({ice: {peerId, candidate}}: any) => { // FIXME: remove once proper types are implemented
@@ -31,15 +39,19 @@ export class GrabberCaptureClient { // FIXME inherit from the other client
         this.socket.on("player_ice", player_ice_handle);
     }
 
-    send_ping(connectionsCount: number, streamTypes: string[]) {
+    onOfferReceived(getAnswer: IOfferReceiveResponder) {
+        this.offerReceiveResponder = getAnswer;
+    }
+
+    sendPing(connectionsCount: number, streamTypes: string[]) {
         this.socket.emit("ping", {ping: {connectionsCount: connectionsCount, streamTypes: streamTypes}});
     }
 
-    send_offer_answer(playerId: string, answer: string) {
+    sendOfferAnswer(playerId: string, answer: string) {
         this.socket.emit("offer_answer", {offerAnswer: {peerId: playerId, answer}});
     }
 
-    send_grabber_ice(peerId: string, candidate: string) {
+    sendGrabberICE(peerId: string, candidate: string) {
         this.socket.emit("grabber_ice", {ice: {peerId, candidate}});
     }
 }
